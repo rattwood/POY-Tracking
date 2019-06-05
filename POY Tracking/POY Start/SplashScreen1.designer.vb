c0469de4c5db5ec6f4a7b0426bbfeca3faf7d64e
rattwood <rattwood@tecknoserve.com>
2019-06-05 19:17:56 +0700

    v26 Release updates

--- POY Tracking/POY Start/SplashScreen1.designer.vb
+++ POY Tracking/POY Start/SplashScreen1.designer.vb
@@ -78,7 +78,7 @@ Partial Class SplashScreen1
         Me.Version.Name = "Version"
         Me.Version.Size = New System.Drawing.Size(241, 30)
         Me.Version.TabIndex = 1
-        Me.Version.Text = "Version 1.0.0.25" & Global.Microsoft.VisualBasic.ChrW(10) & "support@tecknoserve.com"
+        Me.Version.Text = "Version 1.0.0.26" & Global.Microsoft.VisualBasic.ChrW(10) & "support@tecknoserve.com"
         '
         'Copyright
         '
